f2eaf56c5cf3aca262f39872944a87a28d8a335c
tuutti <tuutti@iki.fi>
2021-12-20 12:13:34 +0200

    UHF-3860: Remove helfi specific overrides since they are fixed in base module now

--- src/Flysystem/Azure.php
+++ src/Flysystem/Azure.php
@@ -4,7 +4,6 @@ declare(strict_types = 1);
 
 namespace Drupal\helfi_azure_fs\Flysystem;
 
-use Drupal\helfi_azure_fs\Flysystem\Adapter\AzureBlobStorageAdapter;
 use Drupal\flysystem_azure\Flysystem\Azure as AzureBase;
 
 /**
@@ -15,19 +14,4 @@ use Drupal\flysystem_azure\Flysystem\Azure as AzureBase;
  * @Adapter(id = "helfi_azure")
  */
 final class Azure extends AzureBase {
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getAdapter(): AzureBlobStorageAdapter {
-    try {
-      return new AzureBlobStorageAdapter($this->getClient(), $this->configuration['container']);
-    }
-    catch (\Exception $e) {
-      $this->logger->error($e->getMessage());
-
-      throw $e;
-    }
-  }
-
 }
