216ac08c259620f13d30b545acf3a6858dbf407a
tuutti <tuutti@iki.fi>
2022-07-26 14:08:28 +0300

    UHF-6199: Fixed connection string when using SAS tokens

--- src/Flysystem/Azure.php
+++ src/Flysystem/Azure.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace Drupal\helfi_azure_fs\Flysystem;
 
+use Drupal\flysystem_azure\Flysystem\Adapter\AzureBlobStorageAdapter;
 use Drupal\flysystem_azure\Flysystem\Azure as AzureBase;
 
 /**
@@ -18,18 +19,32 @@ final class Azure extends AzureBase {
   /**
    * {@inheritdoc}
    */
+  public function getAdapter(): AzureBlobStorageAdapter {
+    return new AzureBlobStorageAdapter($this->getClient(), $this->configuration['container']);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function getConnectionString(): string {
-    $values = [
-      'DefaultEndpointsProtocol' => $this->configuration['protocol'],
-      'AccountName' => $this->configuration['name'],
-      'EndpointSuffix' => $this->configuration['endpointSuffix'],
-    ];
 
     if (!empty($this->configuration['token'])) {
-      $values['SharedAccessSignature'] = $this->configuration['token'];
+      $values = [
+        'BlobEndpoint' => vsprintf('%s://%s.blob.%s', [
+          $this->configuration['protocol'],
+          $this->configuration['name'],
+          $this->configuration['endpointSuffix'],
+        ]),
+        'SharedAccessSignature' => $this->configuration['token'],
+      ];
     }
     else {
-      $values['AccountKey'] = $this->configuration['key'];
+      $values = [
+        'DefaultEndpointsProtocol' => $this->configuration['protocol'],
+        'AccountName' => $this->configuration['name'],
+        'EndpointSuffix' => $this->configuration['endpointSuffix'],
+        'AccountKey' => $this->configuration['key'],
+      ];
     }
     $connectionString = '';
 
