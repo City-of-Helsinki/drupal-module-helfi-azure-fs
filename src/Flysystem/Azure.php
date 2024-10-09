a52a4dcaaf2c5544fcaf775d0b643f0a94314deb
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2024-10-09 15:43:32 +0300

    UHF-10544: Generate image styles when requested

--- src/Flysystem/Azure.php
+++ src/Flysystem/Azure.php
@@ -149,15 +149,12 @@ final class Azure implements FlysystemPluginInterface, ContainerFactoryPluginInt
     // We use 'responsive_image' module, so each image can generate up to
     // four derivatives, each taking several seconds.
     // @see https://helsinkisolutionoffice.atlassian.net/browse/UHF-8204
-    if (str_contains($uri, 'styles/') && !file_exists($uri)) {
+    if (str_contains($uri, '/styles/') && !file_exists($uri)) {
       // Return a 'local' image style URL until the image is generated and
       // copied to Azure blob storage. Each derivative is generated when the
       // image style URL is called for the first time, allowing the generation
       // to be decoupled from main request.
-      $localUri = str_replace('azure://', 'public://', $uri);
-
-      return $this->externalUrls[$uri] = UrlHelper::encodePath(
-        $this->fileUrlGenerator->generateString($localUri));
+      return $this->getDownloadUrl($uri);
     }
     $target = $this->getTarget($uri);
 
